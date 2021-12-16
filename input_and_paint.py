892cdbe7103324646c7688d91a27d03c2d82b0d6
Kuzmarg <vovakuz45616@gmail.com>
2021-12-16 12:25:15 +0200

    Update input_and_paint.py

--- input_and_paint.py
+++ input_and_paint.py
@@ -12,7 +12,7 @@ def read_csv(path):
     with open(path, 'r',encoding='utf-8') as file:
         file.readline()
         for line in file:
-            line=line.strip().split(',')
+            line=line.strip().split()
             res[line[0]]=res.get(line[0],[])+[line[1]]
             if not oriented:
                 res[line[1]]=res.get(line[1],[])+[line[0]]
