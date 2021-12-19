e63b083dc05c0f5196041ab96feaf0ec41074740
Kuzmarg <vovakuz45616@gmail.com>
2021-12-19 03:00:13 +0200

    Create input.py

---
+++ input.py
@@ -0,0 +1,18 @@
+def read_csv(path):
+    res={}
+    
+    oriented=input('Your graph is oriented?(y/n)')
+    while oriented not in ['y','n']:
+        oriented=input('Incorrect input. Your graph is oriented?(y/n)')
+    if oriented=='y':
+        oriented=True
+    else:
+        oriented=False
+    
+    with open(path, 'r',encoding='utf-8') as file:
+        for line in file:
+            line=line.strip().split()
+            res[line[0]]=res.get(line[0],[])+[line[1]]
+            if not oriented:
+                res[line[1]]=res.get(line[1],[])+[line[0]]
+    return res
