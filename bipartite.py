318ccdb1280302d276642d7bbc58eaedd427c2b3
Kateryna Akhynko <kateryna.akhynko@ucu.edu.ua>
2021-12-19 17:16:59 +0200

    update bipartite.py

--- bipartite.py
+++ bipartite.py
@@ -6,13 +6,15 @@ start_time = time.time()
 def read_csv(path):
     res={}
 
-    oriented=input('Your graph is oriented?(y/n)')
-    while oriented not in ['y','n']:
-        oriented=input('Incorrect input. Your graph is oriented?(y/n)')
-    if oriented=='y':
-        oriented=True
-    else:
-        oriented=False
+    # oriented=input('Your graph is oriented?(y/n) ')
+    # while oriented not in ['y','n']:
+    #     oriented=input('Incorrect input. Your graph is oriented?(y/n) ')
+    # if oriented=='y':
+    #     oriented=True
+    # else:
+    #     oriented=False
+
+    oriented=False
 
     with open(path, 'r',encoding='utf-8') as file:
         for line in file:
@@ -23,30 +25,60 @@ def read_csv(path):
     return res
 
 
-res = read_csv("C:/Users/HP/Desktop/work/descrete/project/task7_data/task7_data/bipartie/bipartie_5000_3126273_try3.csv")
+res = read_csv("C:/Users/HP/Desktop/work/descrete/project/task7_data/task7_data/bipartie/bipartie_100_1246_try3.csv")
+# print(res)
+print('9', res['9'])
+print('85', res['85'])
+dif= [item for item in res['9'] if item in res['49']]
+print('dif2', [item for item in dif if item in res['85']])
+print('49', res['49'])
 
 
-def two_parts(vert_dict):
-    "Divides all vertices into to parts"
+def bipartite(vert_dict: dict) -> bool:
+    """Divides all vertices of connected non directed graph into two parts
+    and if that can be done, graph is bipartite
+
+    Args:
+        vert_dict: dict of vertices
+
+    Returns:
+        bool: whether the graph is bipartite or not
+
+    >>> bipartite({'a': ['b', 'c'], 'b': ['a', 'd', 'e'], 'c': ['m', 'h', 'a'], 'm': ['c', 't'],\
+    'h': ['c', 'n'], 't': ['m'], 'n': ['h', 'z'], 'z': ['n'], 'd': ['b'], 'e': ['b']})
+    True
+
+    >>> bipartite({'a': ['b', 'c'], 'b': ['a', 'd', 'e'], 'c': ['m', 'h', 'a'], 'm': ['c', 't'],\
+    'h': ['c', 'n'], 't': ['m'], 'n': ['h', 'z'], 'z': ['n'], 'd': ['b'], 'e': ['b', 'd']})
+    False
+
+    """
     red = []
     blue = []
     vertices = list(vert_dict.keys())
     red += vertices[0]
-    for vertice in vert_dict:
+    for vertice in vertices:
         if vertice in red:
             for item in vert_dict[vertice]:
                 if item not in red:
                     blue += vert_dict[vertice]
                 else:
-                    return 'Graph is not Bipartite'
+                    return False
         else:
             for item in vert_dict[vertice]:
                 if item not in blue:
                     red += vert_dict[vertice]
                 else:
-                    return 'Graph is not Bipartite'
-    return 'Graph is Bipartite'
+                    return False
+    return True
 
-print(two_parts(res))
+
+print(bipartite(res))
 
 print("My program took", time.time() - start_time, "to run")
+
+# if __name__ == "__main__":
+#     import doctest
+#     doctest.testmod()
+
+# print(bipartite({'1': ['6'], '2': ['3'], '3': ['6', '1'], '4': ['5'], '7': ['4'], '5': ['2', '3']}))
