e94ecf576a9d2dff9bf964cbbe0af139e105adc1
Kateryna Akhynko <92521245+akhynkokateryna@users.noreply.github.com>
2021-12-19 17:18:17 +0200

    Update bipartite.py

--- bipartite.py
+++ bipartite.py
@@ -1,38 +1,5 @@
 "bipartite.py"
 
-import time
-start_time = time.time()
-
-def read_csv(path):
-    res={}
-
-    # oriented=input('Your graph is oriented?(y/n) ')
-    # while oriented not in ['y','n']:
-    #     oriented=input('Incorrect input. Your graph is oriented?(y/n) ')
-    # if oriented=='y':
-    #     oriented=True
-    # else:
-    #     oriented=False
-
-    oriented=False
-
-    with open(path, 'r',encoding='utf-8') as file:
-        for line in file:
-            line=line.strip().split()
-            res[line[0]]=res.get(line[0],[])+[line[1]]
-            if not oriented:
-                res[line[1]]=res.get(line[1],[])+[line[0]]
-    return res
-
-
-res = read_csv("C:/Users/HP/Desktop/work/descrete/project/task7_data/task7_data/bipartie/bipartie_100_1246_try3.csv")
-# print(res)
-print('9', res['9'])
-print('85', res['85'])
-dif= [item for item in res['9'] if item in res['49']]
-print('dif2', [item for item in dif if item in res['85']])
-print('49', res['49'])
-
 
 def bipartite(vert_dict: dict) -> bool:
     """Divides all vertices of connected non directed graph into two parts
@@ -71,14 +38,3 @@ def bipartite(vert_dict: dict) -> bool:
                 else:
                     return False
     return True
-
-
-print(bipartite(res))
-
-print("My program took", time.time() - start_time, "to run")
-
-# if __name__ == "__main__":
-#     import doctest
-#     doctest.testmod()
-
-# print(bipartite({'1': ['6'], '2': ['3'], '3': ['6', '1'], '4': ['5'], '7': ['4'], '5': ['2', '3']}))
