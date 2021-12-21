ea9bd24ab69fc3a12a96f0d2ebab59e735636157
Kuzmarg <vovakuz45616@gmail.com>
2021-12-21 02:56:17 +0200

    Update bipartite.py

--- bipartite.py
+++ bipartite.py
@@ -1,6 +1,5 @@
 "bipartite.py"
 
-
 def bipartite(vert_dict: dict) -> bool:
     """
     Divides all vertices of connected non directed graph into two parts
@@ -20,42 +19,44 @@ def bipartite(vert_dict: dict) -> bool:
     'h': ['c', 'n'], 't': ['m'], 'n': ['h', 'z'], 'z': ['n'], 'd': ['b'], 'e': ['b', 'd']})
     False
 
+    >>> bipartite({'a':['b'],'c':['d'],'b':['a','k'],'d':['k','c'],'k':['d','b']})
+    True
     """
-    blue = []
-    fuck_up = []
-    red = []
+    blue = set()
+    vert_left = True
+    red = set()
     for vertice in vert_dict:
-        red += vertice
+        red.add(vertice)
         break
-    for vertice in vert_dict:
-        if vertice in red:
-            for item in vert_dict[vertice]:
-                if item not in red:
-                    blue += vert_dict[vertice]
-                else:
-                    return False
-        elif vertice in blue:
-            for item in vert_dict[vertice]:
-                if item not in blue:
-                    red += vert_dict[vertice]
-                else:
-                    return False
-        else:
-            fuck_up += vertice
-
-
-        if len(fuck_up) != 0:
-            for vertice in fuck_up:
-                if vertice in red:
-                    for item in vert_dict[vertice]:
-                        if item not in red:
-                            blue += vert_dict[vertice]
-                        else:
-                            return False
-                elif vertice in blue:
-                    for item in vert_dict[vertice]:
+    while vert_left:
+        vert_left=False
+        for vertice in vert_dict:
+            if vertice in red:
+                for item in vert_dict[vertice]:
+                    if item not in red:
                         if item not in blue:
-                            red += vert_dict[vertice]
-                        else:
-                            return False
+                            blue.add(item)
+                    else:
+                        return False
+            elif vertice in blue:
+                for item in vert_dict[vertice]:
+                    if item not in blue:
+                        if item not in red:
+                            red.add(item)
+                    else:
+                        return False
+            else:
+                vert_left=True
+
     return True
+
+# if __name__=='__main__':
+#     import input
+    # import time
+    # time1=time.time()
+    # graph=input.read_csv('/mnt/c/Users/vovak/Downloads/bipartie_5000_3126273_try3.csv')
+    # print(time.time()-time1)
+    # print(bipartite(graph))
+    # print(time.time()-time1)
+    # import doctest
+    # doctest.testmod()
