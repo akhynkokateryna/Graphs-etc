594f8ae2937f5f4012833a815b1dc4949894b28f
kvitK1 <92575017+kvitK1@users.noreply.github.com>
2021-12-21 19:50:09 +0200

    Update euler.py

--- euler.py
+++ euler.py
@@ -1,9 +1,22 @@
+"""
+Module looking for Euler circuits in graphs.
+"""
 import connection
 
-def from_dict(G):
-    """
-    Returns the list of tuples of ribs, that 
-    is used below for passing all the ribs
+
+def from_dict(G: dict) -> list:
+    """Additional fucntion to look for Euler circuit in graph.
+
+    Args:
+        G (dict): a dictionary with vertexes as keys and
+        lists of adjacent vertexes as values
+
+    Returns:
+        list: the list with edges as tuples
+
+    >>> print(from_dict({1: [2, 5], 2: [1, 5], 3: [4, 5], 4: [3, 5], 5: [1, 2, 3, 4]}))
+    [(1, 2), (1, 5), (2, 1), (2, 5), (3, 4), (3, 5), (4, 3), (4, 5), (5, 1), (5, 2), (5, 3), (5, 4)]
+
     """
     links = []
     for u in G:
@@ -11,11 +24,19 @@ def from_dict(G):
             links.append((u,v))
     return links
 
-def fleury(G):
-    """
-    fleury algorithm
-    >>> fleury({'1': ['2', '5'], '2': ['1', '5'], '3': ['5', '4'], '4': ['3', '5'], '5': ['1', '2', '3', '4']})
+def fleury(G: dict) -> list:
+    """Does Fleury algorithm to find Euler circuit.
+
+    Args:
+        G (dict): a dictionary with vertexes as keys and
+        lists of adjacent vertexes as values
+
+    Returns:
+        list: the list which contains a sequence of vertexes which form Euler circuit
+
+    >>> print(fleury({'1': ['2', '5'], '2': ['1', '5'], '3': ['5', '4'], '4': ['3', '5'], '5': ['1', '2', '3', '4']}))
     ['1', '2', '5', '4', '3', '5', '1']
+
     """
     for item in G:
         if len(G[item]) % 2 != 0:
